aa88d9274dd4b0148938fc43a1b0ec0d866e899a
Alexey <>
2020-01-25 00:15:23 +1100

    Add SnapKit

---
+++ TradeBitcoin/TradeBitcoin/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  TradeBitcoin
+//
+//  Created by Alexey on 24/1/20.
+//  Copyright © 2020 Alexey Zhilnikov. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    let l = UILabel()
+    l.snp.makeConstraints { make in }
+  }
+
+
+}
+
---
+++ TradeBitcoin/TradeBitcoinTests/TradeBitcoinTests.swift
@@ -0,0 +1,34 @@
+//
+//  TradeBitcoinTests.swift
+//  TradeBitcoinTests
+//
+//  Created by Alexey on 24/1/20.
+//  Copyright © 2020 Alexey Zhilnikov. All rights reserved.
+//
+
+import XCTest
+@testable import TradeBitcoin
+
+class TradeBitcoinTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
