556ebf8416c564cf8d111c6ba02fc3f28c98793e
Alexey <>
2020-01-25 01:13:11 +1100

    Remove storyboards

---
+++ TradeBitcoin/TradeBitcoin/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  TradeBitcoin
+//
+//  Created by Alexey on 25/1/20.
+//  Copyright © 2020 Alexey Zhilnikov. All rights reserved.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
