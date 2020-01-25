4300a3d2d5a01a50ec6f7a59e0599859eb2027b5
Alexey Zhilnikov <zhilnikov@hotmail.com>
2020-01-26 02:34:26 +1100

    Add test data and tickers parsing tests

---
+++ TradeBitcoin/TradeBitcoinTests/PriceParsingTests.swift
@@ -0,0 +1,47 @@
+//
+//  PriceParsingTests.swift
+//  TradeBitcoinTests
+//
+//  Created by Alexey on 26/1/20.
+//  Copyright © 2020 Alexey Zhilnikov. All rights reserved.
+//
+
+import XCTest
+@testable import TradeBitcoin
+
+class PriceParsingTests: XCTestCase {
+
+  private let jsonData = TestsHelper.loadJSON(from: "Ticker")!
+  private let decoder = JSONDecoder()
+  private var sut: TickerPrice!
+  
+  override func setUp() {
+    super.setUp()
+    sut = try! decoder.decode(TickerPrice.self, from: jsonData)
+  }
+
+  override func tearDown() {
+    sut = nil
+    super.tearDown()
+  }
+  
+  func testTickersNumber() {
+    XCTAssertEqual(sut.count, 22)
+  }
+  
+  func testLastPriceGBP() {
+    XCTAssertEqual(sut["GBP"]?.last, 6347.54)
+  }
+  
+  func testBuyPriceGBP() {
+    XCTAssertEqual(sut["GBP"]?.buy, 6347.53)
+  }
+  
+  func testSellPriceGBP() {
+    XCTAssertEqual(sut["GBP"]?.sell, 6347.52)
+  }
+  
+  func testSymbolPriceGBP() {
+    XCTAssertEqual(sut["GBP"]?.symbol, "£")
+  }
+}
---
+++ TradeBitcoin/TradeBitcoinTests/TestsHelper/TestsHelper.swift
@@ -0,0 +1,21 @@
+//
+//  TestsHelper.swift
+//  TradeBitcoinTests
+//
+//  Created by Alexey on 26/1/20.
+//  Copyright © 2020 Alexey Zhilnikov. All rights reserved.
+//
+
+import Foundation
+
+class TestsHelper {
+  
+  class func loadJSON(from fileName: String) -> Data? {
+    guard let url = Bundle(for: TestsHelper.self).url(forResource: fileName, withExtension: "json") else {
+      return nil
+    }
+    
+    guard let data = try? Data(contentsOf: url) else { return nil }
+    return data
+  }
+}
